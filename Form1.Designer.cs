331bb1785b86678cf62d832de7427a2400102bd6
Mercenary96 <ben290996@gmail.com>
2015-03-03 14:26:26 +0000

    Initial Commit
    Noughts and Crosses game with simple AI.

---
+++ Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace NoughtsAndCrossesWithAI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.a1 = new System.Windows.Forms.Button();
+            this.a2 = new System.Windows.Forms.Button();
+            this.a3 = new System.Windows.Forms.Button();
+            this.b1 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.c1 = new System.Windows.Forms.Button();
+            this.c2 = new System.Windows.Forms.Button();
+            this.c3 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.instructionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // a1
+            // 
+            this.a1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a1.Location = new System.Drawing.Point(12, 57);
+            this.a1.Name = "a1";
+            this.a1.Size = new System.Drawing.Size(122, 132);
+            this.a1.TabIndex = 0;
+            this.a1.UseVisualStyleBackColor = true;
+            this.a1.Click += new System.EventHandler(this.ButtonClicked);
+            // 
+            // a2
+            // 
+            this.a2.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a2.Location = new System.Drawing.Point(140, 57);
+            this.a2.Name = "a2";
+            this.a2.Size = new System.Drawing.Size(122, 132);
+            this.a2.TabIndex = 1;
+            this.a2.UseVisualStyleBackColor = true;
+            this.a2.Click += new System.EventHandler(this.ButtonClicked);
+            // 
+            // a3
+            // 
+            this.a3.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a3.Location = new System.Drawing.Point(268, 57);
+            this.a3.Name = "a3";
+            this.a3.Size = new System.Drawing.Size(122, 132);
+            this.a3.TabIndex = 2;
+            this.a3.UseVisualStyleBackColor = true;
+            this.a3.Click += new System.EventHandler(this.ButtonClicked);
+            // 
+            // b1
+            // 
+            this.b1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b1.Location = new System.Drawing.Point(12, 195);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(122, 132);
+            this.b1.TabIndex = 3;
+            this.b1.UseVisualStyleBackColor = true;
+            this.b1.Click += new System.EventHandler(this.ButtonClicked);
+            // 
+            // b2
+            // 
+            this.b2.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b2.Location = new System.Drawing.Point(140, 195);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(122, 132);
+            this.b2.TabIndex = 4;
+            this.b2.UseVisualStyleBackColor = true;
+            this.b2.Click += new System.EventHandler(this.ButtonClicked);
+            // 
+            // b3
+            // 
+            this.b3.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b3.Location = new System.Drawing.Point(268, 195);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(122, 132);
+            this.b3.TabIndex = 5;
+            this.b3.UseVisualStyleBackColor = true;
+            this.b3.Click += new System.EventHandler(this.ButtonClicked);
+            // 
+            // c1
+            // 
+            this.c1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.c1.Location = new System.Drawing.Point(12, 333);
+            this.c1.Name = "c1";
+            this.c1.Size = new System.Drawing.Size(122, 132);
+            this.c1.TabIndex = 6;
+            this.c1.UseVisualStyleBackColor = true;
+            this.c1.Click += new System.EventHandler(this.ButtonClicked);
+            // 
+            // c2
+            // 
+            this.c2.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.c2.Location = new System.Drawing.Point(140, 333);
+            this.c2.Name = "c2";
+            this.c2.Size = new System.Drawing.Size(122, 132);
+            this.c2.TabIndex = 7;
+            this.c2.UseVisualStyleBackColor = true;
+            this.c2.Click += new System.EventHandler(this.ButtonClicked);
+            // 
+            // c3
+            // 
+            this.c3.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.c3.Location = new System.Drawing.Point(268, 333);
+            this.c3.Name = "c3";
+            this.c3.Size = new System.Drawing.Size(122, 132);
+            this.c3.TabIndex = 8;
+            this.c3.UseVisualStyleBackColor = true;
+            this.c3.Click += new System.EventHandler(this.ButtonClicked);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(401, 24);
+            this.menuStrip1.TabIndex = 9;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.newGameToolStripMenuItem.Text = "New Game";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.instructionsToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // instructionsToolStripMenuItem
+            // 
+            this.instructionsToolStripMenuItem.Name = "instructionsToolStripMenuItem";
+            this.instructionsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.instructionsToolStripMenuItem.Text = "Instructions";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(401, 472);
+            this.Controls.Add(this.c3);
+            this.Controls.Add(this.c2);
+            this.Controls.Add(this.c1);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b1);
+            this.Controls.Add(this.a3);
+            this.Controls.Add(this.a2);
+            this.Controls.Add(this.a1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button a1;
+        private System.Windows.Forms.Button a2;
+        private System.Windows.Forms.Button a3;
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button c1;
+        private System.Windows.Forms.Button c2;
+        private System.Windows.Forms.Button c3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem instructionsToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NoughtsAndCrossesWithAI
+{
+    public partial class Form1 : Form
+    {
+        bool turn = true; // Turn switches to true for X and false for O
+        string title = "Noughts and Crosses";
+        Random rnd = new Random();
+        int turnCount = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonClicked(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            if (turn == true)
+            {
+                button.Text = "X";
+                button.Enabled = false;
+            }
+            else
+            {
+                button.Text = "O";
+                button.Enabled = false;
+            }
+
+            CheckForWinner();
+            turn = !turn;
+            turnCount++;
+            
+            if (turn == false)
+            {
+                AITurn();
+            }
+        }
+
+        private void InitialiseGame()
+        {
+            MessageBox.Show("Player to go first will now be selected.", title);
+            if (rnd.Next(0, 2) == 0)
+            {
+                MessageBox.Show("Human player goes first.", title);
+            }
+            else
+            {
+                MessageBox.Show("AI player goes first.", title);
+                turn = false;
+                AITurn();
+            }
+        }
+
+        private void AITurn()
+        {
+            Button buttonChosen = new Button();
+
+            Func<Button, bool> ButtonEnabled = (button)
+                =>
+            {
+                if (button.Enabled == true)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            };
+
+            while (true)
+            {
+                switch (rnd.Next(0, 9))
+                {
+                    case 0:
+                        buttonChosen = a1;
+                        break;
+                    case 1:
+                        buttonChosen = a2;
+                        break;
+                    case 2:
+                        buttonChosen = a3;
+                        break;
+                    case 3:
+                        buttonChosen = b1;
+                        break;
+                    case 4:
+                        buttonChosen = b2;
+                        break;
+                    case 5:
+                        buttonChosen = b3;
+                        break;
+                    case 6:
+                        buttonChosen = c1;
+                        break;
+                    case 7:
+                        buttonChosen = c2;
+                        break;
+                    case 8:
+                        buttonChosen = c3;
+                        break;
+                }
+
+                if (ButtonEnabled(buttonChosen) == true)
+                {
+                    buttonChosen.PerformClick();
+                    break;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            
+        }
+
+        private void CheckForWinner()
+        {
+            bool winner = false;
+
+            //horizontal and vertical checks
+            if (((a1.Text == a2.Text) && (a2.Text == a3.Text) && (!a1.Enabled)) || ((a1.Text == b1.Text) && (b1.Text == c1.Text) && (!a1.Enabled)))
+            {
+                winner = true;
+            }
+            else if (((b1.Text == b2.Text) && (b2.Text == b3.Text) && (!b1.Enabled)) || ((a2.Text == b2.Text) && (b2.Text == c2.Text) && (!a2.Enabled)))
+            {
+                winner = true;
+            }
+            else if (((c1.Text == c2.Text) && (c2.Text == c3.Text) && (!c1.Enabled)) || ((a3.Text == b3.Text) && (b3.Text == c3.Text) && (!a3.Enabled)))
+            {
+                winner = true;
+            }
+
+            //Diagonals
+            if ((a1.Text == b2.Text) && (b2.Text == c3.Text) && (!a1.Enabled))
+            {
+                winner = true;
+            }
+            else if ((c1.Text == b2.Text) && (b2.Text == a3.Text) && (!c1.Enabled))
+            {
+                winner = true;
+            }
+
+
+            if (winner)
+            {
+                if (turn == true)
+                {
+                    MessageBox.Show("Player wins!", title);
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("AI Wins!", title);
+                    this.Close();
+                }
+            }
+            else
+            {
+                if (turnCount == 9)
+                {
+                    MessageBox.Show("It was a draw!", "Fail!");
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitialiseGame();
+        }
+    }
+}
